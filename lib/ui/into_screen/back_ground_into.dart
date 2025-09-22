9f606100d7da718663457581332474292ea318f6
moham <mk22443388@gmail.com>
2025-09-22 15:07:21 +0300

    - Refactor BackGroundInto widget - Use final for class members - Add super.key to constructor - Use MediaQuery for responsive padding and spacing - Add conditional rendering for child3

--- lib/ui/into_screen/back_ground_into.dart
+++ lib/ui/into_screen/back_ground_into.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:islamy_app/core/colors/app_color.dart';
-
 import '../../core/images/app_image.dart';
 
 class BackGroundInto extends StatelessWidget {
-  Widget child1;
-  Widget child2;
-  Widget? child3;
-  BackGroundInto({required this.child1,required this.child2,this.child3 = null});
+  final Widget child1;
+  final Widget child2;
+  final Widget? child3;
 
+  const BackGroundInto({
+    super.key,
+    required this.child1,
+    required this.child2,
+    this.child3,
+  });
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * 0.02,
-        ),
+        padding: EdgeInsets.symmetric(horizontal: width * 0.02),
         color: AppColor.backgroundSelect,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Image.asset(AppImage.logo), Flexible(child: child1), Flexible(child: child2), Padding(
+             Image.asset(AppImage.logo),
+            Flexible(child: child1),
+            Flexible(child: child2),
+            if (child3 != null)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
                 child: child3,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 8
-                )),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.08,)
-            
-
+              ),
+            SizedBox(height: height * 0.08),
           ],
         ),
       ),
