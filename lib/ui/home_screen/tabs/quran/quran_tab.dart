4bd6fed6726aa09542751a19fc3365dd9deb19d2
moham <mk22443388@gmail.com>
2025-09-22 14:05:33 +0300

    - Remove unnecessary files and comments - Adjust padding and use const for SizedBox - Update TextFormField styling

--- lib/ui/home_screen/tabs/quran/quran_tab.dart
+++ lib/ui/home_screen/tabs/quran/quran_tab.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:islamy_app/ui/home_screen/tabs/quran/most_recent/most_recent_shared_preferences.dart';
 import 'package:islamy_app/ui/home_screen/tabs/quran/most_recent/shared_prefs_helper.dart';
 import 'package:islamy_app/ui/home_screen/tabs/quran/quran_sources.dart';
 import 'package:islamy_app/ui/home_screen/tabs/quran/sura_item/suras_list.dart';
@@ -12,7 +11,6 @@ import 'most_recent/most_recent_design.dart';
 class QuranTab extends StatefulWidget {
   @override
   State<QuranTab> createState() => _QuranTabState();
-
 }
 
 class _QuranTabState extends State<QuranTab> {
@@ -23,7 +21,7 @@ class _QuranTabState extends State<QuranTab> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 21),
+        const SizedBox(height: 21),
         ConstrainedBox(
           constraints: BoxConstraints(
             maxHeight: MediaQuery.of(context).size.height * 0.065,
@@ -34,17 +32,14 @@ class _QuranTabState extends State<QuranTab> {
             },
             cursorColor: AppColor.gold,
             focusNode: FocusNode(),
-            style: TextStyle(color: AppColor.lightGold),
+            style: const TextStyle(color: AppColor.lightGold),
             decoration: InputDecoration(
               filled: true,
-              fillColor: Color(0xB3202020),
+              fillColor: const Color(0xB3202020),
               focusedBorder: builtTextFormFieldBorder(),
-
               disabledBorder: builtTextFormFieldBorder(),
-
               enabledBorder: builtTextFormFieldBorder(),
-
-              prefixIcon: Padding(
+              prefixIcon:  Padding(
                 padding: EdgeInsets.symmetric(vertical: 15),
                 child: SvgPicture.asset(
                   AppIcon.ic_quran,
@@ -53,27 +48,24 @@ class _QuranTabState extends State<QuranTab> {
                   colorFilter: ColorFilter.mode(AppColor.gold, BlendMode.srcIn),
                 ),
               ),
-
               hintText: 'Sura Name',
-              hintStyle: Theme.of(
-                context,
-              ).textTheme.titleMedium?.copyWith(color: Color(0xFFD9D9D9)),
+              hintStyle: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: const Color(0xFFD9D9D9)),
             ),
           ),
         ),
-
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Text('Most Recently', style: Theme.of(context).textTheme.titleMedium),
-        SizedBox(height: 15),
-
-        MostRecentDesign(),
-        //M
-        SizedBox(height: 10),
+        const SizedBox(height: 15),
+         MostRecentDesign(),
+        const SizedBox(height: 10),
         Text(' Suras List', style: Theme.of(context).textTheme.titleMedium),
         Expanded(
           child: ListView.separated(
             shrinkWrap: true,
-            padding: EdgeInsets.symmetric(vertical: 15),
+            padding: const EdgeInsets.symmetric(vertical: 15),
             itemBuilder: (context, index) {
               return GestureDetector(
                 onTap: () {
@@ -84,12 +76,11 @@ class _QuranTabState extends State<QuranTab> {
                     arguments: searchResultOfSuraList[index],
                   );
                 },
-                //M
                 child: SurasList(index: searchResultOfSuraList[index]),
               );
             },
             separatorBuilder: (context, index) =>
-                Divider(endIndent: 64, thickness: 1.5, indent: 64),
+            const Divider(endIndent: 64, thickness: 1.5, indent: 64),
             itemCount: searchResultOfSuraList.length,
           ),
         ),
@@ -100,7 +91,7 @@ class _QuranTabState extends State<QuranTab> {
   OutlineInputBorder builtTextFormFieldBorder() {
     return OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
-      borderSide: BorderSide(color: AppColor.gold, width: 2),
+      borderSide: const BorderSide(color: AppColor.gold, width: 2),
     );
   }
 
@@ -108,19 +99,15 @@ class _QuranTabState extends State<QuranTab> {
     List<int> addIndexOfSura = [];
     for (int i = 0; i < QuranResources.englishQuranResources.length; i++) {
       if (QuranResources.englishQuranResources[i].toLowerCase().contains(
-            textFormUser.toLowerCase(),
-          ) ||
+        textFormUser.toLowerCase(),
+      ) ||
           QuranResources.arabicQuranResources[i].toLowerCase().contains(
             textFormUser.toLowerCase(),
           )) {
         addIndexOfSura.add(i);
       }
-      else{
-
-      }
     }
-      searchResultOfSuraList = addIndexOfSura;
-    setState(() {
-    });
+    searchResultOfSuraList = addIndexOfSura;
+    setState(() {});
   }
 }
--- lib/ui/home_screen/tabs/quran/sura_item/suras_list.dart
+++ lib/ui/home_screen/tabs/quran/sura_item/suras_list.dart
@@ -9,57 +9,60 @@ class SurasList extends StatelessWidget {
   final int index;
   const SurasList({
     required this.index,
+    super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-       Container(
-         padding: EdgeInsets.symmetric(
-           //M
-           vertical: 12,
-         ),
-         //M
-            width: MediaQuery.of(context).size.width *  0.12,
-
-         decoration: BoxDecoration(
-           image:const DecorationImage(image: AssetImage(AppImage.suraNumber),fit: BoxFit.cover),
-         ),
-         child: Text('${index + 1 }',
-           textAlign: TextAlign.center,
-           style: Theme.of(context).textTheme.titleSmall?.copyWith(
-             color: AppColor.white,
-             fontSize: 12,
-           ),
-         ),
-       ),
-        SizedBox(width: 24,),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12,
+          ),
+          width: MediaQuery.of(context).size.width * 0.12,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AppImage.suraNumber),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Text(
+            '${index + 1}',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+              color: AppColor.white,
+              fontSize: 12,
+            ),
+          ),
+        ),
+        const SizedBox(width: 24),
         Padding(
           padding: const EdgeInsets.only(top: 8),
           child: Column(
-            crossAxisAlignment:CrossAxisAlignment.center ,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(QuranResources.englishQuranResources[index],
-              style: Theme.of(context).textTheme.titleMedium,
+              Text(
+                QuranResources.englishQuranResources[index],
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              //M
-              SizedBox(height:10.01),
-              Text(QuranResources.arabicQuranResources[index],
-              style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                color: AppColor.white
-              ),
+              const SizedBox(height: 10.01),
+              Text(
+                QuranResources.arabicQuranResources[index],
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall
+                    ?.copyWith(color: AppColor.white),
               ),
             ],
           ),
         ),
-        Spacer(),
-        Text(QuranResources.arabicQuranResources[index],
-        style: Theme.of(context).textTheme.titleMedium,
-        )
+        const Spacer(),
+        Text(
+          QuranResources.arabicQuranResources[index],
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ],
     );
   }
 }
-
-
